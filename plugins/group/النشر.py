f33d40d1c9e5acea60a1ac6d6b19f30e5b77645f
MoHaMaD BaQiR <103285401+R0RR7@users.noreply.github.com>
2025-03-28 20:21:06 +0300

    Create النشر.py

---
+++ plugins/group/النشر.py
@@ -0,0 +1,50 @@
+"""
+
+❃ `{i}نشر` <الوقت بين الارسال> <عدد الارسال> <الرسالة>
+    لـ نشر الرسالة بعدد معين في جيمع مجموعاتك لكن بين ارسال رسالة واخرى وقت معين جرب ارسل   `. انشرلي 2 5 مرحبا`
+   
+❃ `{i}ايقاف النشر`
+   لـ ايقاف امر النشر في المجموعات
+
+⚠️ أنتبه قد يؤدي استخدام هذا الامر بكثرة الى تقييد حسابك من مراسلة المستخدمين اذا قاموا بالتبليغ عنك.. 
+"""
+
+import asyncio
+from .. import jmthon_cmd,jmdB, DEV_CHAT
+
+@jmthon_cmd(pattern="نشر")
+async def gcast(event):
+    args = event.text.split(" ", 3)
+    delay = float(args[1])
+    count = int(args[2])
+    msg = str(args[3])
+
+    mirz = await event.eor("⌔∮ يتم النشر في جميع المجموعات الان")
+    er = 0
+    done = 0
+    jmdB.set_key("NSHR", True)
+
+    for i in range(count):
+        if not jmdB.get_key("NSHR"):
+            break
+        async for x in event.client.iter_dialogs():
+            if x.is_group:
+                chat = x.id
+                try:
+                    if chat not in DEV_CHAT:
+                        await event.client.send_message(chat, msg)
+                        await asyncio.sleep(1)
+                        done += 1
+                except BaseException:
+                    er += 1
+        await mirz.edit(f"**⌔∮  تم بنجاح النشر الى ** `{done}` **من الدردشات ، خطأ في ارسال الى ** `{er}` **من الدردشات**")
+        await asyncio.sleep(delay)
+
+
+@jmthon_cmd(pattern="ايقاف (النشر|نشر)")
+async def stop_spam(e):
+    if jmdB.get_key("NSHR"):
+        jmdB.del_key("NSHR")
+        await e.respond("**⌔∮ تم إيقاف النشر الوقتي بنجاح!**")
+    else:
+        await e.respond("**⌔∮أمر النشر ليس قيد التنفيذ حاليًا.**")
