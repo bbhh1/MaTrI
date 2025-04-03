9f66886ec6b252f8f30cdab1971b40166c6eb9fe
MoHaMaD BaQiR <103285401+R0RR7@users.noreply.github.com>
2025-04-04 01:28:34 +0300

    Update session.py

--- session.py
+++ session.py
@@ -1,18 +1,16 @@
-##Pyrogram string session
+#!/usr/bin/env python3
+# (c) https://t.me/TelethonChat/37677
+# This Source Code Form is subject to the terms of the GNU
+# General Public License, v.3.0. If a copy of the GPL was not distributed with this
+# file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.en.html.
 
-import asyncio
-from pyrogram import Client
+from telethon.sessions import StringSession
+from telethon.sync import TelegramClient
+
 
 APP_ID = int(input("Enter APP ID here: "))
 API_HASH = input("Enter API HASH here: ")
 
-with Client(":memory:", api_id=APP_ID, api_hash=API_HASH) as app:
-    app.send_message(
-            "me",
-            "**هذا هو كود السيشن الخاص بك يستعمل للتنصيب احذر مشاركته لأشخاص ليسوا أهل ثقة قد يتسبب بأختراق حسابك**\n\n"
-            f"`{app.export_session_string()}`"
-        )
-    print(
-            "Done, your Pyrogram session string has been sent to "
-            "Saved Messages of your Telegram account!"
-        )
+with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
+    print(client.session.save())
+    client.sendmessage("me", client.session.save())
