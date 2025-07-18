0c7e377b58602ef3bf94b19deeb6a423e39d9418
bbhh1 <112596831+bbhh1@users.noreply.github.com>
2025-07-18 06:53:50 +0300

    Update logger.py

--- jmthon/core/logger.py
+++ jmthon/core/logger.py
@@ -44,11 +44,11 @@ with contextlib.suppress(ImportError):
 LOGS.info(
     """
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-        بدء تشغيل سورس جمثـون   
+        بدء تشغيل سورس مـاتركي   
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
     """
 )
 
 LOGS.info(f"إصدار البايثون ➤ {platform.python_version()}")
 LOGS.info(f"إصدار مكتبة التيليثون ➤ {__version__} [الطبقة: {LAYER}]")
-LOGS.info(f"إصدار جمثون ➤ 1.0.0 [{platform.system()}]")
+LOGS.info(f"إصدار ماتركس ➤ 1.0.0 [{platform.system()}]")
