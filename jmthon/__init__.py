24a06a5a83b61e8b1ff6ad8761d1789838beb95e
bbhh1 <112596831+bbhh1@users.noreply.github.com>
2025-07-30 01:42:06 +0300

    Update __init__.py

--- jmthon/__init__.py
+++ jmthon/__init__.py
@@ -1,6 +1,11 @@
+
+# مشروع ماتركس - نسخة معدلة
+
 import logging
 import time
 import sys
+import os
+from dotenv import load_dotenv
 import jmthon.core.ubclient
 from .config import Var
 from .core.client import JmthonClient
@@ -9,22 +14,22 @@ from .core.session import both_session
 from .core.logger import *
 from database import jmdB, JmdB
 
+load_dotenv()  # تحميل متغيرات البيئة من ملف .env
+
 version = "1.0.0"
 start_time = time.time()
-bot_token = JmdB.get_config("BOT_TOKEN")
 
+bot_token = os.getenv("BOT_TOKEN")  # قراءة التوكن من ملف .env
 
 jmubot = jmthon_bot = JmthonClient(
-        session=StringSession(str(Var.SESSION)),
-        app_version=version,
-        device_model="Jmthon",
-       )
-
+    session=StringSession(str(Var.SESSION)),
+    app_version=version,
+    device_model="Jmthon",
+)
 
 tgbot = asst = JmthonClient("Tgbot", bot_token=bot_token)
 
 del bot_token
-
 
 HNDLR = jmdB.get_key("HNDLR") or "."
 SUDO_HNDLR = jmdB.get_key("SUDO_HNDLR") or HNDLR
